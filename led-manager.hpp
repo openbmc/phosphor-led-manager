b2238191d12467371164da18ab3a2b251e88c9f9
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2016-10-17 17:46:37 +0530

    Add LED grouping support
    This enables creating custom groups and participating LEDs so that it can later
    be generated from MRW. For each of the group, a dbus object is created which
    will announce LED actions.
    
    Fixes openbmc/openbmc#550
    
    Change-Id: I7a56d08755288dcfce45ee4c6d6b6c5e5aa454f7
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

---
+++ led-manager.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <map>
+#include <set>
+#include <sdbusplus/bus.hpp>
+
+namespace phosphor
+{
+
+namespace led
+{
+
+/** @class Manager
+ *  @brief Manages group of LEDs and applies action on the elements of group
+ */
+
+class Manager
+{
+private:
+
+    /** @brief Define possible actions on a given LED.
+     *  For the BLINK operation, follow 50-50 duty cycle
+     */
+    enum Action
+    {
+        OFF,
+        ON,
+        BLINK,
+    };
+
+    // Dbus constructs used by LED manager
+    sdbusplus::bus::bus iv_bus;
+
+public:
+
+    Manager(const char*, const char*, const char*);
+    Manager() = delete;
+    ~Manager() = default;
+    Manager(const Manager&) = delete;
+    Manager& operator=(const Manager&) = delete;
+    Manager(Manager&&) = default;
+    Manager& operator=(Manager&&) = default;
+
+    /** @brief Name of the LED and it's proposed action.
+     *  This structure is supplied as configuration at build time
+     */
+    struct LedAction
+    {
+        std::string name;
+        Action action;
+
+        // Needed for inserting elements into sets
+        bool operator<(const LedAction& right) const
+        {
+            return name < right.name;
+        }
+
+        // Needed for set union / intersect
+        bool operator==(const LedAction& right) const
+        {
+            // Only if name and action are same, consider equal
+            if (name == right.name &&
+                    action == right.action)
+            {
+                return true;
+            }
+            return false;
+        }
+    };
+
+    // A static global map is created at once
+    static std::map<std::string,
+           std::set<LedAction>> cv_LedMap;
+
+    void run();
+};
+
+} // namespace led
+
+} // namespace phosphor
