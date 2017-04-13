7d9828fbdbaff3b0aefaf566720ab2b8f67d9c28
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2017-04-13 00:19:44 -0500

    Class and stub for fru fault monitor application
    Change-Id: I92ead0c8d3132dd7a6740b536231d6588ac42471
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

---
+++ fault-monitor/fru-fault-monitor.hpp
@@ -0,0 +1,137 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server.hpp>
+#include "../config.h"
+
+namespace phosphor
+{
+namespace led
+{
+namespace inventory
+{
+namespace fault
+{
+namespace monitor
+{
+
+/** @brief Assert or deassert an LED based on the input FRU
+  *  @param[in] bus       -  The Dbus bus object
+  *  @param[in] path      -  Inventory path of the FRU
+  *  @param[in] assert    -  Assert if true deassert if false
+  */
+void action(sdbusplus::bus::bus& bus,
+            const std::string& path,
+            const bool assert);
+
+class Remove;
+
+/** @class Add
+ *  @brief Implementation of LED handling during FRU fault
+ *  @details Handles LED assertion based on the FRU fault
+ */
+class Add
+{
+    public:
+        Add() = delete;
+        ~Add() = default;
+        Add(const Add&) = delete;
+        Add& operator=(const Add&) = delete;
+        Add(Add&&) = default;
+        Add& operator=(Add&&) = default;
+
+        /** @brief constructs Add
+         *  @param[in] bus -  The Dbus bus object
+         */
+        Add(sdbusplus::bus::bus& bus):
+            matchCreated(
+                bus,
+                "type='signal',"
+                "interface='org.freedesktop.DBus.ObjectManager',"
+                "member='InterfacesAdded',"
+                "path='/xyz/openbmc_project/logging'",
+                created,
+                this)
+        {
+            //Do nothing
+        }
+    private:
+
+        /** @brief sdbusplus signal match for fault created */
+        sdbusplus::server::match::match matchCreated;
+
+        std::vector<std::unique_ptr<Remove>> removeWatch;
+
+        /** @brief Callback function for fru fault created
+         *  @param[in] msg       - Data associated with subscribed signal
+         *  @param[in] data      - Pointer to this object instance
+         *  @param[out] retError - Error returned
+         *  @return zero on success and error code upon failure
+         */
+        static int created(sd_bus_message* msg,
+                           void* data,
+                           sd_bus_error* retError);
+};
+
+class Remove
+{
+    public:
+        Remove() = delete;
+        ~Remove() = default;
+        Remove(const Remove&) = delete;
+        Remove& operator=(const Remove&) = delete;
+        Remove(Remove&&) = default;
+        Remove& operator=(Remove&&) = default;
+
+        /** @brief constructs Remove
+         *  @param[in] bus  -  The Dbus bus object
+         *  @param[in] path -  Inventory path to fru
+         */
+        Remove(sdbusplus::bus::bus& bus, const std::string& path):
+            inventoryPath(path),
+            matchRemoved(
+                bus,
+                match(path).c_str(),
+                removed,
+                this)
+        {
+            //Do nothing
+        }
+
+    private:
+
+        /** @brief unit name */
+
+        std::string inventoryPath;
+        /** @brief sdbusplus signal matches for fault removed */
+        sdbusplus::server::match::match matchRemoved;
+
+        /** @brief Callback function for fru fault created
+         *  @param[in] msg       - Data associated with subscribed signal
+         *  @param[in] data      - Pointer to this object instance
+         *  @param[out] retError - Error returned
+         *  @return zero on success and error code upon failure
+         */
+        static int removed(sd_bus_message* msg,
+                           void* data,
+                           sd_bus_error* retError);
+
+        /** @brief function to create fault remove match for a fru
+         *  @param[in] unit  - Name of the faulty unit
+         */
+        std::string match(const std::string& path)
+        {
+            std::string matchStmt =
+                "type='signal',"
+                "interface='org.freedesktop.DBus.Properties',"
+                "member='PropertiesChanged',"
+                "path='" + path +
+                "/" + CALLOUT_REV_ASSOCIATION + "'";
+            return matchStmt;
+        }
+};
+}//namespace monitor
+}//namespace fault
+}//namespace inventory
+}//namespace led
+}//namespace phosphor
