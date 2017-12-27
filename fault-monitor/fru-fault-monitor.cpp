606bd415fc43be65a1e68cf88d0257e2a16bd2b5
Marri Devender Rao <devenrao@in.ibm.com>
2017-12-27 03:18:32 -0600

    Generate local elog-errors header file
    Use elog-gen.py, elog-gen-template.mako.hpp, error
    yaml files from SDK to build local elog-errors.hpp file
    
    Change-Id: I3062e3223fd65fd3833ca56aebf59d872370c7cf
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- fault-monitor/fru-fault-monitor.cpp
+++ fault-monitor/fru-fault-monitor.cpp
@@ -1,9 +1,8 @@
 #include <phosphor-logging/elog.hpp>
 #include "xyz/openbmc_project/Led/Fru/Monitor/error.hpp"
 #include "xyz/openbmc_project/Led/Mapper/error.hpp"
-#include "elog-errors.hpp"
 #include "fru-fault-monitor.hpp"
-#include <phosphor-logging/elog-errors.hpp>
+#include "phosphor-logging/elog-errors.hpp"
 #include "xyz/openbmc_project/Common/error.hpp"
 
 namespace phosphor
