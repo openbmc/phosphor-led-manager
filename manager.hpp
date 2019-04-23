6fd9e440be73865d99cd408e0f87ec639882267b
tony lee <tony.lee@quantatw.com>
2019-04-23 09:09:15 +0800

    Update the value of Period to D-bus obtained from configuration YAML.
    Update the value of Period to D-bus obtained from configuration YAML
    when the target group was asserted. The logical behavior
    of Period is as same as Dutyon.
    
    Change-Id: I4b064f2f7321479cabc7deeaa57779b0a8cc3d21
    Signed-off-by: tony lee <tony.lee@quantatw.com>

--- manager.hpp
+++ manager.hpp
@@ -140,9 +140,10 @@ class Manager
      *  @param[in]  objPath   -  dbus object path
      *  @param[in]  action    -  Intended action to be triggered
      *  @param[in]  dutyOn    -  Duty Cycle ON percentage
+     *  @param[in]  period    -  Time taken for one blink cycle
      */
     void drivePhysicalLED(const std::string& objPath, Layout::Action action,
-                          uint8_t dutyOn);
+                          uint8_t dutyOn, const uint16_t period);
 
     /** @brief Makes a dbus call to a passed in service name.
      *  This is now the physical LED controller
