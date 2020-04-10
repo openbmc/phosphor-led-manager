e11b85dea8db3396bba5282aaed170cac1529592
George Liu <liuxiwei@inspur.com>
2020-04-10 11:23:52 +0800

    Update led-manager to use JSON
    Current LED manager uses compile time generated led-gen.hpp and creates
    DBus objects for the groups.
    We would need this changed to use the generated JSON.
    
    Tested: JSON used at runtime when --enable-json given at configure time.
    led-gen.hpp not created when using JSON and led-gen.hpp created by
    default when using YAML at build time.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I781f8cb090ece8b87730e6c97795624282857c64

---
+++ json-config.hpp
@@ -0,0 +1,104 @@
+#include "config.h"
+
+#include "ledlayout.hpp"
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+namespace fs = std::filesystem;
+
+using Json = nlohmann::json;
+using LedAction = std::set<phosphor::led::Layout::LedAction>;
+using LedMap = std::map<std::string, LedAction>;
+
+using InternalFailure =
+    sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+
+static const Json empty{};
+
+/** @brief Parse LED JSON file and output Json object
+ *
+ *  @param[in] path - path of LED JSON file
+ *
+ *  @return Json - Json object
+ */
+inline Json readJson(const std::string& path)
+{
+    fs::path dir(path);
+    if (!fs::exists(dir) || fs::is_empty(dir))
+    {
+        throw std::invalid_argument("Wrong file path");
+    }
+
+    std::ifstream jsonFile(path);
+    if (!jsonFile.is_open())
+    {
+        std::cerr << "Error opening LED JSON file, PATH=" << path << "\n";
+        throw InternalFailure();
+    }
+
+    return Json::parse(jsonFile);
+}
+
+/** @brief Returns action enum based on string
+ *
+ *  @param[in] action - action string
+ *
+ *  @return Action - action enum (On/Blink)
+ */
+phosphor::led::Layout::Action getAction(const std::string& action)
+{
+    assert(action == "On" || action == "Blink");
+
+    return action == "Blink" ? phosphor::led::Layout::Blink
+                             : phosphor::led::Layout::On;
+}
+
+LedMap loadJsonConfig()
+{
+    LedMap ledMap{};
+    try
+    {
+        auto json = readJson(LED_JSON_FILE);
+        auto leds = json.value("leds", empty);
+        for (const auto& entry : leds)
+        {
+            fs::path tmpPath(std::string{OBJPATH});
+            tmpPath /= entry.value("group", "");
+            auto objpath = tmpPath.string();
+            auto members = entry.value("members", empty);
+
+            LedAction ledActions{};
+            for (const auto& member : members)
+            {
+                auto name = member.value("name", "");
+
+                // Since only have Blink/On, provided support to override the
+                // default Blink priority.
+                auto action = getAction(member.value("Action", "Blink"));
+                uint8_t dutyOn = member.value("DutyOn", 50);
+                uint16_t period = member.value("Period", 0);
+
+                // Since only have Blink/On, provided support to override the
+                // default Blink priority.
+                auto priority = getAction(member.value("Priority", "Blink"));
+                phosphor::led::Layout::LedAction ledAction{name, action, dutyOn,
+                                                           period, priority};
+                ledActions.emplace(ledAction);
+            }
+
+            // Generated an std::map of LedGroupNames to std::set of LEDs
+            // containing the name and properties.
+            ledMap.emplace(objpath, ledActions);
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return ledMap;
+}
