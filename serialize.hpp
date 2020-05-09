92b65c6945f0fd899fce0e5a41d8a1482bbd5100
George Liu <liuxiwei@inspur.com>
2020-05-09 11:26:35 +0800

    Add storeGroups/restoreGroups method to LED Manager
    Use CEREAL to storeGroup/restoreGroups the current state of asserted groups.
    Call storeGroups() when the request comes to add to(remove from) asserted
    group.
    Call restoreGroups() as part of starting LED Manager daemon.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: Ibeb1de5f51e3d67e98eeea34764e9efc6d6d8b35

---
+++ serialize.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <set>
+
+namespace phosphor
+{
+namespace led
+{
+
+using AssertedStateGroups = std::set<std::string>;
+
+/** @class Serialize
+ *  @brief Store and restore groups of LEDs
+ */
+class Serialize
+{
+  public:
+    /** @brief Refer the user supplied LED layout and sdbusplus handler
+     *
+     */
+    Serialize()
+    {
+        restoreGroups();
+    }
+
+    /** @brief Store asserted state groups
+     *
+     *  @param [in] group     - asserted state groups
+     *  @param [in] asserted  - true or false, the default is false
+     */
+    void storeGroups(std::string group, bool asserted = false);
+
+    /** @brief Restore asserted state groups
+     *
+     */
+    void restoreGroups();
+
+    /** @brief Get asserted state groups
+     *
+     */
+    AssertedStateGroups getAssertedStateGroups();
+
+  private:
+    /** @brief the set of asserted state groups*/
+    AssertedStateGroups assertedStateGroups;
+};
+
+} // namespace led
+} // namespace phosphor
