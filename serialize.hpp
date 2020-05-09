790e28d1fdefd979484910cbb30bd2cdca986a59
George Liu <liuxiwei@inspur.com>
2020-05-09 11:26:35 +0800

    Add storeGroups/restoreGroups method to LED Manager
    Use CEREAL to storeGroup/restoreGroups the current state of
    asserted groups.
    Call storeGroups() when the request comes to add to(remove from)
    asserted group.
    Call restoreGroups() as part of starting LED Manager daemon.
    
    Tested: Manually set the Asserted property of each group to true,
    after rebooting, all property values tested with the busctrl command
    are still ture.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: Ibeb1de5f51e3d67e98eeea34764e9efc6d6d8b35

---
+++ serialize.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <filesystem>
+#include <fstream>
+#include <set>
+#include <string>
+
+namespace phosphor
+{
+namespace led
+{
+
+namespace fs = std::filesystem;
+
+// the set of names of asserted groups, which contains the D-Bus Object path
+using SavedGroups = std::set<std::string>;
+
+/** @class Serialize
+ *  @brief Store and restore groups of LEDs
+ */
+class Serialize
+{
+  public:
+    Serialize(const fs::path& path) : path(path)
+    {
+        restoreGroups();
+    }
+
+    /** @brief Store asserted group names to
+     * /var/lib/phosphor-led-manager/savedGroups
+     *
+     *  @param [in] group     - asserted state groups
+     *  @param [in] asserted  - asserted state, true or false
+     */
+    void storeGroups(const std::string& group, bool asserted);
+
+    /** @brief restore asserted group names from
+     *         /var/lib/phosphor-led-manager/savedGroups
+     */
+    void restoreGroups();
+
+    /** @brief Is there a group save state in SAVED_GROUPS_FILE
+     *
+     *  @param [in] objPath - The Dbus path that hosts LED group
+     *
+     *  @return             - true: exist, false: does not exist
+     */
+    bool getGroupSavedState(const std::string& objPath);
+
+  private:
+    /** @brief the set of names of asserted groups */
+    SavedGroups savedGroups;
+
+    /** @brief the path of file for storing the names of asserted groups */
+    fs::path path;
+};
+
+} // namespace led
+} // namespace phosphor
