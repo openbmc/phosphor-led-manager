748bfe9708fcd4247af997cbe660b510d6820047
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2020-10-08 07:14:15 -0500

    Add a utility script
    the script will set Asserted property of all the D-Bus objects
    under /xyz/openbmc_project/led/groups/ to true or false
    
    Change-Id: I8e14d78f15f97094054a9864d5c4291b6814238b
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

---
+++ scripts/led-set-all-groups-asserted.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This shell script sets all the group D-Bus objects
+# /xyz/openbmc_project/led/groups/ to true or false.
+
+function usage()
+{
+    echo "set-all-group.sh [true/false]"
+    return 0;
+}
+
+# User passed in argument
+action=$1
+
+#We need exactly 1 argument
+if [ $# -ne 1 ]; then
+    echo "Need ONE argument passed";
+    usage;
+    exit -1;
+fi
+
+# If it is not "true" or "false", exit
+if [ $action != "true" ] && [ $action != "false" ]; then
+    echo "Bad argument $action passed";
+    usage;
+    exit -1;
+fi
+
+# Now, set the LED group to what has been requedsted
+for line in $(busctl tree xyz.openbmc_project.LED.GroupManager | grep -e groups/ | grep -v bmc_booted | grep -v power_on | awk -F 'xyz' '{print "/xyz" $2}');
+do
+    busctl set-property xyz.openbmc_project.LED.GroupManager $line xyz.openbmc_project.Led.Group Asserted b $action;
+done
+
+#Return Success
+exit 0
