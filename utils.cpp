d97125176382e2321e74f31f9fadb22f77ec316b
George Liu <liuxiwei@inspur.com>
2020-10-16 09:07:02 +0800

    Add utility for D-Bus
    Putting all D-Bus related operation to the utils file.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I934e519bca43b73da01ed09fc73f6c7cdc795e1f

---
+++ utils.cpp
@@ -0,0 +1,104 @@
+#include "utils.hpp"
+
+namespace phosphor
+{
+namespace led
+{
+namespace utils
+{
+
+/** Get service name */
+std::string DBusHandler::getService(const char* path,
+                                    const char* interface) const
+{
+    using InterfaceList = std::vector<std::string>;
+    std::map<std::string, std::vector<std::string>> mapperResponse;
+
+    auto& bus = DBusHandler::getBus();
+
+    auto mapper = bus.new_method_call(MAPPER_BUSNAME, MAPPER_OBJ_PATH,
+                                      MAPPER_IFACE, "GetObject");
+    mapper.append(path, InterfaceList({interface}));
+
+    auto mapperResponseMsg = bus.call(mapper);
+    mapperResponseMsg.read(mapperResponse);
+
+    return mapperResponse.cbegin()->first;
+}
+
+/** Get property and return the value of the property*/
+PropertyValue DBusHandler::getProperty(const char* objPath,
+                                       const char* dbusProp,
+                                       const char* interface) const
+{
+    auto& bus = DBusHandler::getBus();
+    auto service = getService(objPath, interface);
+    auto method = bus.new_method_call(service.c_str(), objPath,
+                                      DBUS_PROPERTY_IFACE, "Get");
+    method.append(interface, dbusProp);
+
+    PropertyValue value{};
+    auto reply = bus.call(method);
+    reply.read(value);
+
+    return value;
+}
+
+/** Get subtree paths and return the vector of the path*/
+Paths DBusHandler::getSubTreePaths(const std::string& objPath,
+                                   const std::vector<std::string>& intfs,
+                                   int depth)
+{
+    auto& bus = DBusHandler::getBus();
+    auto mapperCall = bus.new_method_call(MAPPER_BUSNAME, MAPPER_OBJ_PATH,
+                                          MAPPER_IFACE, "GetSubTreePaths");
+
+    mapperCall.append(objPath);
+    mapperCall.append(depth);
+    mapperCall.append(intfs);
+    auto reply = bus.call(mapperCall);
+
+    Paths paths;
+    reply.read(paths);
+
+    return paths;
+}
+
+/** Get subtree and return the map of the object*/
+ObjectTree DBusHandler::getSubTree(const std::string& objPath,
+                                   const std::vector<std::string>& intfs,
+                                   int depth)
+{
+    auto& bus = DBusHandler::getBus();
+    auto mapperCall = bus.new_method_call(MAPPER_BUSNAME, MAPPER_OBJ_PATH,
+                                          MAPPER_IFACE, "GetSubTree");
+
+    mapperCall.append(objPath);
+    mapperCall.append(depth);
+    mapperCall.append(intfs);
+    auto reply = bus.call(mapperCall);
+
+    ObjectTree objectTree;
+    reply.read(objectTree);
+
+    return objectTree;
+}
+
+/** Set property */
+void DBusHandler::setProperty(const std::string& objectPath,
+                              const std::string& interface,
+                              const std::string& propertyName,
+                              const PropertyValue& value) const
+{
+    auto& bus = DBusHandler::getBus();
+    auto service = getService(objectPath.c_str(), interface.c_str());
+    auto method = bus.new_method_call(service.c_str(), objectPath.c_str(),
+                                      DBUS_PROPERTY_IFACE, "Set");
+    method.append(interface.c_str(), propertyName.c_str(), value);
+
+    bus.call_noreply(method);
+}
+
+} // namespace utils
+} // namespace led
+} // namespace phosphor
---
+++ utils.hpp
@@ -0,0 +1,112 @@
+#pragma once
+#include <sdbusplus/server.hpp>
+
+#include <map>
+#include <vector>
+namespace phosphor
+{
+namespace led
+{
+namespace utils
+{
+constexpr auto MAPPER_BUSNAME = "xyz.openbmc_project.ObjectMapper";
+constexpr auto MAPPER_OBJ_PATH = "/xyz/openbmc_project/object_mapper";
+constexpr auto MAPPER_IFACE = "xyz.openbmc_project.ObjectMapper";
+constexpr auto DBUS_PROPERTY_IFACE = "org.freedesktop.DBus.Properties";
+
+// The value of the property(type: variant, contains some basic types)
+using PropertyValue = std::variant<uint8_t, uint16_t, std::string>;
+
+// The vector of subtree paths
+using Paths = std::vector<std::string>;
+
+// The map of subtree object
+using ObjectTree =
+    std::map<std::string, std::map<std::string, std::vector<std::string>>>;
+
+/**
+ *  @class DBusHandler
+ *
+ *  Wrapper class to handle the D-Bus calls
+ *
+ *  This class contains the APIs to handle the D-Bus calls.
+ */
+class DBusHandler
+{
+  public:
+    /** @brief Get the bus connection. */
+    static auto& getBus()
+    {
+        static auto bus = sdbusplus::bus::new_default();
+        return bus;
+    }
+
+    /**
+     *  @brief Get service name by the path and interface of the DBus.
+     *
+     *  @param[in] path      -  D-BUS object path
+     *  @param[in] interface -  D-BUS Interface
+     *
+     *  @return std::string  -  the D-Bus service name
+     *
+     *  @throw sdbusplus::exception::SdBusError when it fails
+     */
+    std::string getService(const char* path, const char* interface) const;
+
+    /** @brief Get property(type: variant)
+     *
+     *  @param[in] objPath      -   The D-Bus object path
+     *  @param[in] dbusProp     -   The property name to get
+     *  @param[in] interface    -   The D-Bus interface
+     *
+     *  @return The value of the property(type: variant)
+     *
+     *  @throw sdbusplus::exception::SdBusError when it fails
+     */
+    PropertyValue getProperty(const char* objPath, const char* dbusProp,
+                              const char* interface) const;
+
+    /** @brief Get sub tree paths by the path and interface of the DBus.
+     *
+     *  @param[in]  objPath   -  D-BUS object path
+     *  @param[in]  intfs     -  The vector of interface
+     *  @param[in]  depth     -  Default is 0 to search all
+     *
+     *  @return Paths vector of subtree paths
+     *
+     *  @throw sdbusplus::exception::SdBusError when it fails
+     */
+    Paths getSubTreePaths(const std::string& objPath,
+                          const std::vector<std::string>& intfs, int depth = 0);
+
+    /** @brief Get sub tree by the path and interface of the DBus.
+     *
+     *  @param[in]  objPath   -  D-BUS object path
+     *  @param[in]  intf      -  D-BUS object interface
+     *  @param[in]  depth     -  Default is 0 to search all
+     *
+     *  @return ObjectTree map of subtree
+     *
+     *  @throw sdbusplus::exception::SdBusError when it fails
+     */
+    ObjectTree getSubTree(const std::string& objPath,
+                          const std::vector<std::string>& intfs, int depth = 0);
+
+    /** @brief Set D-BUS property
+     *
+     *  @param[in] objectPath       -   D-Bus object path
+     *  @param[in] interface        -   D-Bus interface
+     *  @param[in] propertyName     -   D-Bus property name
+     *  @param[in] value            -   The value to be set
+     *
+     *  @throw sdbusplus::exception::SdBusError when it fails
+     */
+    void setProperty(const std::string& objectPath,
+                     const std::string& interface,
+                     const std::string& propertyName,
+                     const PropertyValue& value) const;
+};
+
+} // namespace utils
+} // namespace led
+} // namespace phosphor
