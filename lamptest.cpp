6b2b07f76eaf53c47f97084e0e3f955a12e5e72d
George Liu <liuxiwei@inspur.com>
2020-11-23 16:40:14 +0800

    Update all physical LEDs of lamp test class
    Add the getSubTreePaths method for utils and update all physical LEDs of
    lamp test class.
    
    Get sub tree paths via the physical path and interfaces and traverse all
    paths and set all physical LEDs to On when we called the start method.
    Set all physical LEDs to Off when we called the stopLampTest method.
    Set the Asserted property to false when the timer expires.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I27727db62fe963a1a7c8b391d73801bcaf9efeec

--- lamptest.cpp
+++ lamptest.cpp
@@ -2,6 +2,8 @@
 
 #include "lamptest.hpp"
 
+#include <phosphor-logging/log.hpp>
+
 namespace phosphor
 {
 namespace led
@@ -10,21 +12,47 @@ namespace led
 void LampTest::stop()
 {
     timer.setEnabled(false);
+
+    // Set all the Physical action to Off
+    for (const auto& path : physicalLEDPaths)
+    {
+        manager.drivePhysicalLED(path, Layout::Action::Off, 0, 0);
+    }
 }
 
 void LampTest::start()
 {
     // restart lamp test, it contains initiate or reset the timer.
     timer.restart(std::chrono::seconds(LAMP_TEST_TIMEOUT_IN_SECS));
+
+    // Set all the Physical action to On for lamp test
+    for (const auto& path : physicalLEDPaths)
+    {
+        manager.drivePhysicalLED(path, Layout::Action::On, 0, 0);
+    }
 }
 
 void LampTest::timeOutHandler()
 {
+    using namespace phosphor::logging;
+
     // set the Asserted property of lamp test to false
+    if (!groupObj)
+    {
+        log<level::ERR>("the Group object is nullptr");
+        throw std::runtime_error("the Group object is nullptr");
+    }
+
+    groupObj->asserted(false);
 }
 
-void LampTest::requestHandler(bool value)
+void LampTest::requestHandler(Group* group, bool value)
 {
+    if (groupObj == NULL)
+    {
+        groupObj = std::move(group);
+    }
+
     if (value)
     {
         start();
