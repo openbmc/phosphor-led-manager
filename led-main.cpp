f3dc874e514e0f7df51a9d3662e59d89bb83989c
George Liu <liuxiwei@inspur.com>
2021-02-18 10:50:24 +0800

    led-use-json: Use specific config files
    When enabled `led-use-json`, it should use the JsonConfig class to
    find the JSON file name, and do not start until JSON config is found.
    
    If it has to wait for the IBMCompatible interface to show up on D-bus
    first, it will wait for that.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I1528a3d3711ef6b604868387ad42cb3c0afde119

--- led-main.cpp
+++ led-main.cpp
@@ -3,6 +3,7 @@
 #include "group.hpp"
 #ifdef LED_USE_JSON
 #include "json-config.hpp"
+#include "json-parser.hpp"
 #else
 #include "led-gen.hpp"
 #endif
@@ -20,15 +21,40 @@
 
 int main(void)
 {
+
+#ifdef LED_USE_JSON
+    fs::path path{};
+
+    {
+        // Get a new Dbus
+        auto busP = sdbusplus::bus::new_bus();
+
+        // Get a new event loop
+        auto eventP = sdeventplus::Event::get_new();
+
+        // Attach the bus to sd_event to service user requests
+        busP.attach_event(eventP.get(), SD_EVENT_PRIORITY_IMPORTANT);
+        phosphor::led::JsonConfig jsonConfig(busP, eventP);
+
+        while (!jsonConfig.completed())
+        {
+            eventP.loop();
+        }
+
+        path = jsonConfig.getConfFile();
+
+        // Detach the bus from its sd_event event loop object
+        busP.detach_event();
+    }
+
+    auto systemLedMap = loadJsonConfig(path);
+#endif
+
     // Get a default event loop
     auto event = sdeventplus::Event::get_default();
 
     /** @brief Dbus constructs used by LED Group manager */
     auto& bus = phosphor::led::utils::DBusHandler::getBus();
-
-#ifdef LED_USE_JSON
-    auto systemLedMap = loadJsonConfig(LED_JSON_FILE);
-#endif
 
     /** @brief Group manager object */
     phosphor::led::Manager manager(bus, systemLedMap);
