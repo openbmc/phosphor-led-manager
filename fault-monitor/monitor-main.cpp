39a805469b111506dfbc545adda1ba8d95d9dfe0
George Liu <liuxiwei@inspur.com>
2021-04-01 15:02:57 +0800

    fault-monitor: Add OperationalStatus Functional monitoring
    - Currently, the default way is to update the Asserted property of
      the LedManager by monitoring the `/xyz/openbmc_project/logging`
      object path.
    
    - The intent behind this commit is to add another way to monitor the
      `xyz.openbmc_project.State.Decorator.OperationalStatus` interface
      of the Inventory D-Bus object, and check whether the Inventory
      D-Bus object is associated with the LED group D-Bus object, and
      then update the Asserted property of the LedManager.
    
    - The first way is supported by default. To trigger the second way,
      need to enable `monitor-oper-status`.
    
    Tested:
    - enable `monitor-oper-status` in bbappend file, built successfully.
    
    - Before changing the `OperationalStatus` interface:
      busctl get-property xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory/system/chassis/motherboard/powersupply1 xyz.openbmc_project.State.Decorator.OperationalStatus Functional
      b true
    
      busctl get-property xyz.openbmc_project.LED.GroupManager /xyz/openbmc_project/led/groups/powersupply1_fault xyz.openbmc_project.Led.Group Asserted
      b false
    
      busctl get-property xyz.openbmc_project.LED.Controller.front_psu /xyz/openbmc_project/led/physical/front_psu xyz.openbmc_project.Led.Physical State
      s "xyz.openbmc_project.Led.Physical.Action.Off"
    
    - After changing the `OperationalStatus` interface:
      busctl set-property xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory/system/chassis/motherboard/powersupply1 xyz.openbmc_project.State.Decorator.OperationalStatus Functional b false
    
      busctl get-property xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory/system/chassis/motherboard/powersupply1 xyz.openbmc_project.State.Decorator.OperationalStatus Functional
      b false
    
      busctl get-property xyz.openbmc_project.LED.GroupManager /xyz/openbmc_project/led/groups/powersupply1_fault xyz.openbmc_project.Led.Group Asserted
      b true
    
      busctl get-property xyz.openbmc_project.LED.Controller.front_psu /xyz/openbmc_project/led/physical/front_psu xyz.openbmc_project.Led.Physical State
      s "xyz.openbmc_project.Led.Physical.Action.Blink"
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I36c47198168028ea2b55804cf66d0285847afbd4

--- fault-monitor/monitor-main.cpp
+++ fault-monitor/monitor-main.cpp
@@ -1,11 +1,21 @@
+#include "config.h"
+
+#ifdef MONITOR_OPER_STATUS
+#include "oper-status-monitor.hpp"
+#else
 #include "fru-fault-monitor.hpp"
+#endif
 
 int main(void)
 {
     /** @brief Dbus constructs used by Fault Monitor */
     sdbusplus::bus::bus bus = sdbusplus::bus::new_default();
 
+#ifdef MONITOR_OPER_STATUS
+    phosphor::led::oper::status::monitor::Monitor operStatus(bus);
+#else
     phosphor::led::fru::fault::monitor::Add monitor(bus);
+#endif
     /** @brief Wait for client requests */
     while (true)
     {
