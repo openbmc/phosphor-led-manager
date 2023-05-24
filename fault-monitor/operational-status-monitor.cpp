fcf3a9d08e475b6ae325de4e8ab384f8104f54b1
Priyanga Ramasamy <priyanga24@in.ibm.com>
2023-05-24 02:37:43 -0500

    LED:Invalid association for fault LED group
    The LED fault association group name has been changed from
    fault_led_group to fault_identifying as a part of this change
    <https://github.com/openbmc/phosphor-dbus-interfaces/commit
    /e7c10bd5cdab6cd898e5cad0daab8c1128adc0c8>.
    
    The current code points to the invalid group name. This commit
    has the fix for it.
    
    Test:
    
    busctl set-property xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory/system/chassis/motherboard/vdd_vrm1 xyz.openbmc_project.State.Decorator.OperationalStatus Functional b false
    
    busctl set-property xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory/system/chassis/motherboard/ebmc_card_bmc xyz.openbmc_project.State.Decorator.OperationalStatus Functional b true
    
    Before the fix: Error traces are found when we try to set Functional property.
    phosphor-fru-fault-monitor[1180]: Failed to get endpoints property, ERROR = sd_bus_call: xyz.openbmc_project.Common.Error.ResourceNotFound: The resource is not found., PATH = /xyz/openbmc_project/inventory/system/chassis/motherboard/vdd_vrm1/fault_led_group
    May 31 02:17:32 rain127bmc phosphor-fru-fault-monitor[1180]: The inventory D-Bus object is not associated with the LED group D-Bus object. INVENTORY_PATH = /xyz/openbmc_project/inventory/system/chassis/motherboard/vdd_vrm1
    
    After the fix:
    No error traces are found and Functional property has been updated.
    
    Change-Id: Ib42c1b496b07ca238e2ba3573064d865e4911711
    Signed-off-by: Priyanga Ramasamy <priyanga24@in.ibm.com>

--- fault-monitor/operational-status-monitor.cpp
+++ fault-monitor/operational-status-monitor.cpp
@@ -58,8 +58,8 @@ void Monitor::matchHandler(sdbusplus::message_t& msg)
 const std::vector<std::string>
     Monitor::getLedGroupPaths(const std::string& inventoryPath) const
 {
-    // Get endpoints from the rType
-    std::string faultLedAssociation = inventoryPath + "/fault_led_group";
+    // Get endpoints from fType
+    std::string faultLedAssociation = inventoryPath + "/fault_identifying";
 
     // endpoint contains the vector of strings, where each string is a Inventory
     // D-Bus object that this, associated with this LED Group D-Bus object
