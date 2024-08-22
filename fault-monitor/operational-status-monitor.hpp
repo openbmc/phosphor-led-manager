79d88c356a90c0aba55038ef688dcf3e98d7aaf5
George Liu <liuxiwei@ieisystem.com>
2024-08-22 16:01:44 +0800

    clang-tidy: Enable modernize-avoid-bind check
    The check finds uses of std::bind and boost::bind and replaces them
    with lambdas.
    
    Lambdas will use value-capture unless reference capture is explicitly
    requested with std::ref or boost::ref.
    
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>
    Change-Id: I4491650a46eaab1588474b26efc622e89232ef02

--- fault-monitor/operational-status-monitor.hpp
+++ fault-monitor/operational-status-monitor.hpp
@@ -47,8 +47,7 @@ class Monitor
                     "sender='xyz.openbmc_project.Inventory.Manager', "
                     "arg0namespace='xyz.openbmc_project.State.Decorator."
                     "OperationalStatus'",
-                    std::bind(std::mem_fn(&Monitor::matchHandler), this,
-                              std::placeholders::_1))
+                    [this](sdbusplus::message_t& m) { matchHandler(m); })
 
     {}
 
