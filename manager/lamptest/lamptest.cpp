8f538d9c915a0668243dbcd0550aa28b07678936
George Liu <liuxiwei@ieisystem.com>
2024-08-22 15:41:22 +0800

    Add clang-tidy to phosphor-led-manager
    This commit enables clang-tidy and adds few tidy checks.
    
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>
    Change-Id: I3a7b41545fef4c726b56aeab3e4b4000dfcac32f

--- manager/lamptest/lamptest.cpp
+++ manager/lamptest/lamptest.cpp
@@ -219,7 +219,7 @@ void LampTest::start()
         fs::create_directories(ledDirectory);
     }
 
-    std::ofstream(lampTestIndicator.c_str());
+    std::ofstream ofs(lampTestIndicator.c_str());
 
     // Set all the Physical action to On for lamp test
     for (const auto& path : physicalLEDPaths)
